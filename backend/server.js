2e3d880fdeb867cfc80b64f97bf485501b1a4b26
Satyam Gupta <satgupta23@gmail.com>
2025-04-11 01:26:44 +0530

    project comleted, build project

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,26 @@
 import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./db/db.js";
-import productRoutes from "./routes/product.route.js"
-import cors from "cors"
-
-const app = express()
-
-app.use(cors())
-
+import productRoutes from "./routes/product.route.js";
+import cors from "cors";
+import path from "path";
 dotenv.config();
 
-app.use(express.json());
-app.use("/api/products", productRoutes)
+const app = express();
+const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
-const port = process.env.PORT || 5000
+app.use(cors());
+app.use(express.json());
+app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
 app.listen(port, () => {
   connectDB();
   console.log(`server started at port at http://localhost:${port}`);
