d75c2422be93d0517bb14a2f3be39601d72ada35
Satyam Gupta <satgupta23@gmail.com>
2025-04-11 18:50:28 +0530

    added cors origin

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}))
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.get("/", (req, res) => {
